a0418d48766a6c80cceabbf8109a82dbcfcb29e3
Jeff <jeff@shepptech.com>
2023-09-19 17:33:45 -0400

    Added index details and sidebar menu

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,26 @@
-// Custom JavaScript code
-// For example, smooth scroll:
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
+// Event listener for DOMContentLoaded
+document.addEventListener("DOMContentLoaded", function() {
+  const content = document.getElementById("content");
+
+  const links = document.querySelectorAll("#sidebar a");
+  links.forEach(link => {
+    link.addEventListener("click", function(e) {
       e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-      });
+      loadContent(this.id);  // Function to load new content based on clicked link
+    });
   });
+});
+
+// Placeholder function to load new content based on clicked link
+function loadContent(id) {
+  // Your code to replace content goes here. This could involve fetching
+  // another HTML file or otherwise replacing the content of the "content" div.
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  fetch('pages/about-me.html')
+    .then(response => response.text())
+    .then(text => {
+      document.getElementById('about-me-section').innerHTML = text;
+    });
 });
